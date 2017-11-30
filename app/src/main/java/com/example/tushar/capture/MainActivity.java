004fc4324199debf64831db447379efe45b3c18d
Tushar Moraye <tusharmoraye@gmail.com>
2017-11-30 19:00:10 +0530

    updated imports

--- app/src/main/java/com/example/tushar/capture/MainActivity.java
+++ app/src/main/java/com/example/tushar/capture/MainActivity.java
@@ -1,24 +1,16 @@
 package com.example.tushar.capture;
 
-import android.app.Activity;
-import android.content.ContentValues;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 

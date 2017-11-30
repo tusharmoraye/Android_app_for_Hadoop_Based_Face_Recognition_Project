329cf2dc14f218a1288711ea78247703f3e99261
tusharmoraye <tusharmoraye@gmail.com>
2017-11-30 19:03:52 +0530

    trial commit

--- app/src/main/java/com/example/tushar/capture/ShowImageActivity.java
+++ app/src/main/java/com/example/tushar/capture/ShowImageActivity.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import java.io.IOException;
 
 public class ShowImageActivity extends AppCompatActivity {
